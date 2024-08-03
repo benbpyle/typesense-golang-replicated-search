f1f037d6951a7eaabd4d143c9b7b9e9c89b743f0
Benjamen Pyle <benbpyle@gmail.com>
2024-08-03 16:06:38 -0500

    Initial commit

---
+++ test/golang-typesense.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GolangTypesense from '../lib/golang-typesense-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/golang-typesense-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GolangTypesense.GolangTypesenseStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
